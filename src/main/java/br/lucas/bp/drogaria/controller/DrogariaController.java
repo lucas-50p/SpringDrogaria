6185596f61e89d8868eefd70a25cea5bd6c58ec1
lucas bueno <lucas-50p>
2020-06-05 15:08:32 -0300

    Estratégias para a Geração de Tabelas

--- src/main/java/br/lucas/bp/drogaria/controller/DrogariaController.java
+++ src/main/java/br/lucas/bp/drogaria/controller/DrogariaController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 /*Class que vai gerar serviços
  * caminho Path()
  * quando for chamar esse serviço ("/drogaria")
- * DevTolls: Ferramenta de auto-carregamento, precisa add com botão direito no project procurar spring edit starters*/
+ * DevTolls: Ferramenta de auto-carregamento, precisa add com botão direito no project procurar spring edit starters
+ * application.properties:Update--- inserir(incluir algo new) um novo atributo para entidade, inseriu uma chave estrangeira
+ * application.properties:Validate--- (remover, editar)trocou o tamanho do campo, trocou a chave estrangeira, atualizou uma prapriedade da entidade*/
 @RestController//Controlador
 @RequestMapping("/drogaria")//Caminho do controlador
 public class DrogariaController {
