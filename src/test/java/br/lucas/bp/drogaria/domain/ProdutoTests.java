326c5c932fa9968acd283eeead377540a7001a9a
lucas bueno <lucas-50p>
2020-06-05 11:00:11 -0300

    JUnit test

---
+++ src/test/java/br/lucas/bp/drogaria/domain/ProdutoTests.java
@@ -0,0 +1,24 @@
+package br.lucas.bp.drogaria.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/*Produtotests testa o produto*/
+@SpringBootTest
+public class ProdutoTests {
+	@Test
+	public void criar() {
+		Produto p = new Produto(
+			Short.valueOf("1"),
+			"Coca Cola",
+			Byte.valueOf("15"),
+			BigDecimal.valueOf(10.50),
+			LocalDate.of(2011, 11, 23)
+		);
+		
+		System.out.println(p);
+	}
+}
