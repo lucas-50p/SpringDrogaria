ae5e847fcf86dfeff7e3c445fe89e4133f8dcbff
lucas bueno <lucas-50p>
2020-06-05 12:23:11 -0300

    Camada de repositório

---
+++ src/main/java/br/lucas/bp/drogaria/repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package br.lucas.bp.drogaria.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.lucas.bp.drogaria.domain.Produto;
+
+/*extends quando eu quero fazer herança
+ * JpaRepository: Ele remete a persistencia generica que costuma codificar antigamente
+ * Ele tem o buscar, buscarAll,findAllById: encontrar todos pela chave primaria, salvar......
+ * Short: chave primaria do produto*/
+public interface ProdutoRepository extends JpaRepository<Produto, Short>{
+
+}
