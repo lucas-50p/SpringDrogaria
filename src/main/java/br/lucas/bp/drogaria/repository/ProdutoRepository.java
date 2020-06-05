7a2e5558257932d468185134b9cf950fb2f9ec2e
lucas bueno <lucas-50p>
2020-06-05 12:37:11 -0300

    Teste da Camada Repositório(Produto)

--- src/main/java/br/lucas/bp/drogaria/repository/ProdutoRepository.java
+++ src/main/java/br/lucas/bp/drogaria/repository/ProdutoRepository.java
@@ -7,7 +7,7 @@ import br.lucas.bp.drogaria.domain.Produto;
 /*extends quando eu quero fazer herança
  * JpaRepository: Ele remete a persistencia generica que costuma codificar antigamente
  * Ele tem o buscar, buscarAll,findAllById: encontrar todos pela chave primaria, salvar......
- * Short: chave primaria do produto*/
+ * Short: chave primaria do produto.Class*/
 public interface ProdutoRepository extends JpaRepository<Produto, Short>{
 
 }
---
+++ src/test/java/br/lucas/bp/drogaria/repository/ProdutoRepositoryTests.java
@@ -0,0 +1,30 @@
+package br.lucas.bp.drogaria.repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import br.lucas.bp.drogaria.domain.Produto;
+
+@SpringBootTest
+public class ProdutoRepositoryTests {
+	/*null: estou persistindo no banco no preciso da  chave primaria*/
+	@Autowired//Gerenciar os obj, vai injetar uma instância nesse obj
+	private ProdutoRepository ProdutoRepository;
+	
+	@Test
+	public void inserir() {
+		Produto p1 = new Produto(null, "Coca Cola", Byte.valueOf("15"), BigDecimal.valueOf(10.50),
+				LocalDate.of(2011, 11, 23));
+		Produto p2 = new Produto(null,
+				"Skoll", Byte.valueOf("23"), 
+				BigDecimal.valueOf(8.00),
+				LocalDate.of(2022, 4, 1));
+		
+		ProdutoRepository.save(p1);
+		ProdutoRepository.save(p2);
+	}
+}
