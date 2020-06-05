e684063824a0634de709e685e00ee259d8eccedc
lucas bueno <lucas-50p>
2020-06-05 15:52:39 -0300

    Inserção do Controlador da Categoria

--- src/main/java/br/lucas/bp/drogaria/controller/CategoriaController.java
+++ src/main/java/br/lucas/bp/drogaria/controller/CategoriaController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,15 @@ public class CategoriaController {
 	public List<Categoria> listar(){
 		List<Categoria>categorias = categoriaRepository.findAll();
 		return categorias;
+	}
+	
+	/*Esse metodo --- Pós salvo como se eu tivesse fazendo insert faço um select para buscar
+	 *S: do save retorna o obj após o salvamento retorna obj atualizado
+	 *@RequestBody:Atravez do corpo da requesição vai vir um json tem que ser mapeado nesse obj -
+	 *mapeamento dos dados da requesição, de forma automatica*/
+	@PostMapping()//Método de envio
+	public Categoria inserir(@RequestBody Categoria categoria) {//Pré salvamento
+		Categoria categoriaSalva = categoriaRepository.save(categoria);//pós salvamento
+		return categoriaSalva;
 	}
 }
