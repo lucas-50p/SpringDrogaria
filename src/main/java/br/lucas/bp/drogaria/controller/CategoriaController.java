97168d98dcd5e4835e66fc31887e05f5119452a8
lucas bueno <lucas-50p>
2020-06-08 12:09:12 -0300

    Edição do Controlador da Categoria

--- src/main/java/br/lucas/bp/drogaria/controller/CategoriaController.java
+++ src/main/java/br/lucas/bp/drogaria/controller/CategoriaController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -61,5 +62,11 @@ public class CategoriaController {
 		Categoria categoriaRetorno = categoria.get();
 		return categoriaRetorno;
 		
+	}
+	/*Put sinaliza que voçê quer fazer uma edição*/
+	@PutMapping
+	public Categoria editar(@RequestBody Categoria categoria) {
+		Categoria categoriaEditadada = categoriaRepository.save(categoria);
+		return categoriaEditadada;
 	}
 }
